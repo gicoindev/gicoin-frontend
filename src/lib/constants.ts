88dcb60acf73667113c664b2f7434d96ef166220
gicoindev <gicoindev@gmail.com>
2025-12-13 01:51:58 +0700

    update constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,9 @@
 export const AIRDROP_ADDRESS = "0xE4A9a0a40468EFC73c5ab64fC4E86C765eFaB4Dd";
 
 // kalau nanti ada token utama (misalnya GIC), bisa tambahkan juga
-export const GICOIN_ADDRESS = "0x226a72C33cbc9cfbB8f4af3f528254B5BF303579";
+export const GICOIN_ADDRESS =
+  process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ||
+  "0xe4a9a0a40468efc73c5ab64fc4e86c765efab4dd";
 
 // chain config (opsional, kalau pakai wagmi config khusus)
 export const CHAIN_ID = Number(process.env.NEXT_PUBLIC_CHAIN_ID);
