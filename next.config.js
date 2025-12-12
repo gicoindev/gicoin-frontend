108fcf066a486154d30d0d7c6f8641893372a5a7
gicoindev <gicoindev@gmail.com>
2025-12-12 18:45:47 +0700

    update : next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,27 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
   trailingSlash: true,
 
   turbopack: {
+    // FIX: prevent imports that break Turbopack
     resolveAlias: {
       "@react-native-async-storage/async-storage": "./async-storage-web.js",
-
-      // ⬇⬇ FIX: Prevent pino & thread-stream from bundling on client
       "pino": false,
       "pino-pretty": false,
       "thread-stream": false,
     },
+
+    // FIX: prevent Turbopack from scanning test/bench folders in node_modules
+    exclude: [
+      "**/node_modules/**/test/**",
+      "**/node_modules/**/tests/**",
+      "**/node_modules/**/bench/**",
+      "**/node_modules/**/benchmark/**",
+      "**/*.md",
+      "**/*.markdown",
+      "**/*.zip"
+    ],
   },
 
   typescript: { ignoreBuildErrors: true },
