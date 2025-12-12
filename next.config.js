5eb227ac2b5e7fd833e8df8a1f31a04881fcbade
gicoindev <gicoindev@gmail.com>
2025-12-12 17:11:15 +0700

    fix: disable turbopack for netlify

--- next.config.js
+++ next.config.js
@@ -1,18 +1,25 @@
-const path = require("path");
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
+  trailingSlash: true,
+
+  // ⬇⬇ ADD THIS TO FORCE WEBPACK MODE ⬇⬇
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@react-native-async-storage/async-storage": path.resolve(
+      "@react-native-async-storage/async-storage": require("path").resolve(
         __dirname,
         "async-storage-web.js"
       ),
     };
     return config;
   },
-  eslint: { ignoreDuringBuilds: true },
+
+  // ⬇⬇ THIS LINE IS REQUIRED FOR NEXT 15+ TO DISABLE TURBOPACK ⬇⬇
+  experimental: {
+    webpackBuildWorker: true, // force webpack instead of turbopack
+  },
+
   typescript: { ignoreBuildErrors: true },
 };
 
